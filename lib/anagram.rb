a23ed74512d8e155d8a624da7951032fab283bf7
Brittney Ramirez <brittneyramirez79@gmail.com>
2020-01-06 03:23:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+class Anagram
+  attr_reader:diaper
+  
+  def initialize (anagram)
+    anagram.new = diaper
+    
+    
+    def match 
+      []
+    
+    
+    
+    
+    
+    
+    
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
